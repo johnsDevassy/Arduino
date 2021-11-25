51e26da51bfc592369a5f006c68490f1819ecfaf
johnsDevassy <johns2nd98@gmail.com>
2021-11-26 00:43:42 +0530

    some additions

--- All test/src/main.cpp
+++ All test/src/main.cpp
@@ -1,26 +1,50 @@
 #include <Arduino.h>
 #define onboard 13
 
-int apin = A0;
-int greenPin = 5;
-int redPin = 6;
+int readb1;
+int readb2;
+int buttonPin1 = 12;
+int buttonPin2 = 8;
+int redPin = 9;
+int oldbutton = 0;
+int ledState = 0;
+int bright = 0;
+int buzzPin = 4;
 void setup()
 {
-  pinMode(apin, INPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2,INPUT);
   Serial.begin(9600);
   pinMode(redPin,OUTPUT);
-  pinMode(greenPin,OUTPUT);
+  pinMode(buzzPin,OUTPUT);
 }
 
 void loop()
 {
-  int photoval = analogRead(apin);
-  if(photoval>100){
-    digitalWrite(greenPin,HIGH);
-    digitalWrite(redPin,LOW);
-  }else{
-    digitalWrite(greenPin,LOW);
-    digitalWrite(redPin,HIGH);
+  readb1 = digitalRead(buttonPin1);
+  readb2 = digitalRead(buttonPin2);
+  Serial.println("b1:"+(String)readb1+" "+ "b2:"+ (String)readb2);
+  if(readb1 == 0){
+    bright += 5;
+    if (bright > 255){
+      bright = 255;
+      digitalWrite(buzzPin,HIGH);
+      delay(2000);
+      digitalWrite(buzzPin,LOW);
+    }
+    analogWrite(redPin,bright);
+
   }
-  Serial.println(photoval);
+  else if(readb2 == 0){
+    bright -= 5;
+    if (bright < 0){
+      bright = 0;
+      digitalWrite(buzzPin,HIGH);
+      delay(2000);
+      digitalWrite(buzzPin,LOW);
+    }
+    analogWrite(redPin,bright);
+
+  }
+  delay(25);
 }
